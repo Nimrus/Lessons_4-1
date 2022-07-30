b8041dac2fb42086fdcdde0b949dc8e2a2db554b
Nimrus <59753998+Nimrus@users.noreply.github.com>
2022-07-30 10:51:36 +0800

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,18 @@
+const string = test;
+console.log ("string",string, typeof string);
+const number = 20;
+console.log ("number",number, typeof number);
+const boolean = true ;
+console.log ("boolean",boolean, typeof boolean);
+const trt = null;
+console.log ("trt",trt,typeof trt);
+const undefined = test;
+console.log ("undefined",undefined, typeof undefined);
+const object = {
+name : "carBoo"
+}
+console.log ("object", object, typeof object);
+const symbol = ("id");
+console.log ("symbol",symbol,typeof symbol);
+const biglnt = 9877663
+console.log ("biglnt", biglnt, typeof biglnt);
